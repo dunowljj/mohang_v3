48c5e671032ae350ce6c97925e7d9c3736693511
JungJoon Lee <uuufg3322>
2021-11-18 15:58:32 +0900

    real start! jhye mian

--- MoHang/src/main/java/org/mohang/controller/MainController.java
+++ MoHang/src/main/java/org/mohang/controller/MainController.java
@@ -2,13 +2,14 @@ package org.mohang.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/")
+@Controller
+@RequestMapping("/")
 public class MainController {
 	
 	@GetMapping("/Main")
 	public String main(){
-		System.out.println("gfdads");
 		
 		return "Main";
 	}
