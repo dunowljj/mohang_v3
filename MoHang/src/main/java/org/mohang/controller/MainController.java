5382569137442fccd7e70ffcbbe485682f855175
JungJoon Lee <uuufg3322>
2021-11-18 15:29:32 +0900

    finally start!

--- MoHang/src/main/java/org/mohang/controller/MainController.java
+++ MoHang/src/main/java/org/mohang/controller/MainController.java
@@ -1,61 +1,15 @@
-/*package org.mohang.controller;
+package org.mohang.controller;
 
-import java.io.IOException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import mohang.action.Action;
-import mohang.action.ActionForward;
-import mohang.action.main.MainAction;
-
-
-@WebServlet("/Main/*")
-public class MainController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-  
-    public MainController() {
-        super();
-        
-    }
-    public void doProcess(HttpServletRequest request, HttpServletResponse response)throws ServletException,IOException{
-    	String requestURI = request.getRequestURI();
-    	String contextPath = request.getContextPath(); 
-    	String command = requestURI.substring(contextPath.length()+6);
-    	Action action = null; 
-    	ActionForward forward = null;
-    	if(command.equals("MoHang.do")) {
-    		action = new MainAction();
-    		try {
-				forward = action.execute(request, response);	
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-    	}
-    	
-    	if(forward !=null) {
-    		if(forward.isRedirect()) {
-    			response.sendRedirect(forward.getPath());
-    		}else {
-    			RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
-    			dispatcher.forward(request, response);
-    		}
-    	}
-    }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
-	}
-
+@Controller("/")
+public class MainController {
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
+	@GetMapping("/Main")
+	public String main(){
+		System.out.println("gfdads");
+		
+		return "Main";
 	}
-
 }
-*/
